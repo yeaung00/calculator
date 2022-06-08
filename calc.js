ec448bc9fb7b4b90c0061669016ab690ee8b6895
yeaung00 <yep.aung00@gmail.com>
2022-06-07 18:15:50 -0700

    Added small display

--- calc.js
+++ calc.js
@@ -1,13 +1,16 @@
 const container = document.querySelector(".container");
 const btn = document.querySelectorAll(".btn");
 const display = document.querySelector(".display");
+const smallerDisplay = document.querySelector(".display-small");
 let sign = false;
-let temp = "";
+let smallNumString = "";
+let smallTemp = "";
 let numString = "";
 let numInt = 0;
 let firstNum = 0;
 let isAdding = false;
 let isSubtracting = false;
+
 [].forEach.call(btn, function(item, i) {
     item.addEventListener('click', (e) => {
         getNumber(item.id, e);
@@ -19,6 +22,8 @@ function operate(input, e) {
     }
     else if (input == "clear") {
         display.textContent = 0;
+        smallerDisplay.textContent = 0;
+        smallNumString = "";
         numString = "";
     }
     else if (input == "add") {
@@ -44,10 +49,17 @@ function getNumber(input, e) {
     if (e.target.className != "btn operation") {
         temp = input.toString();
         numString += temp;
+        smallNumString += temp;
         numInt = parseInt(numString,10);
+        smallerDisplay.textContent = smallNumString;
         display.textContent = numString;
     }
-    else { operate(input,e); }
+    else {
+        temp = e.target.innerText;
+        smallNumString += temp;
+        smallerDisplay.textContent = smallNumString;
+        operate(input,e);
+    }
 }
 function add(x,y) {
     return x+y;
@@ -60,6 +72,8 @@ function equal() {
     if (isAdding === true) { numInt = add(firstNum,numInt); }
     else if (isSubtracting === true) { numInt = subtract(firstNum,numInt); }
     display.textContent = numInt;
+    smallerDisplay.textContent = numInt;
+    
 }
 function toggleSign() {
     if (sign === false) { 
