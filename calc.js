6485d010111218bcf15a031defca5da204a91628
yeaung00 <yep.aung00@gmail.com>
2022-06-07 17:55:01 -0700

    Added addition and subtraction functions

--- calc.js
+++ calc.js
@@ -2,23 +2,65 @@ const container = document.querySelector(".container");
 const btn = document.querySelectorAll(".btn");
 const display = document.querySelector(".display");
 let sign = false;
-
-[].forEach.call(btn, function(item) {
+let temp = "";
+let numString = "";
+let numInt = 0;
+let firstNum = 0;
+let isAdding = false;
+let isSubtracting = false;
+[].forEach.call(btn, function(item, i) {
     item.addEventListener('click', (e) => {
-        operate(item.id);
+        getNumber(item.id, e);
     });
   });
-function operate(input) {
+function operate(input, e) {
     if (input == "sign") {
-        toggleSign
+        toggleSign;
     }
-    if (input == "clear") {
+    else if (input == "clear") {
         display.textContent = 0;
+        numString = "";
     }
-    else { display.textContent = input; }
-    
+    else if (input == "add") {
+        firstNum = numInt;
+        display.textContent = 0;
+        numString = "";
+        isSubtracting = false;
+        isAdding = true;
+    }
+    else if (input == "subtract") {
+        firstNum = numInt;
+        display.textContent = 0;
+        numString = "";
+        isAdding = false;
+        isSubtracting = true;
+    }
+    else if (input == "equal") {
+        console.log('equal');
+        equal();
+    }
 }
-
+function getNumber(input, e) {
+    if (e.target.className != "btn operation") {
+        temp = input.toString();
+        numString += temp;
+        numInt = parseInt(numString,10);
+        display.textContent = numString;
+    }
+    else { operate(input,e); }
+}
+function add(x,y) {
+    return x+y;
+}
+function subtract(x,y) {
+    firstNum = x-y;
+    return x-y;
+}
+function equal() {
+    if (isAdding === true) { numInt = add(firstNum,numInt); }
+    else if (isSubtracting === true) { numInt = subtract(firstNum,numInt); }
+    display.textContent = numInt;
+}
 function toggleSign() {
     if (sign === false) { 
         sign = true;
